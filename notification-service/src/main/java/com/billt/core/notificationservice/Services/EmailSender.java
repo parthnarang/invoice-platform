654eafc80f06552c9b7ad434b8882fb63dd9c0fd
parthnarang <parthnarang2201@gmail.com>
2019-01-25 00:15:36 +0530

    solving email config

--- notification-service/src/main/java/com/billt/core/notificationservice/Services/EmailSender.java
+++ notification-service/src/main/java/com/billt/core/notificationservice/Services/EmailSender.java
@@ -1,28 +1,15 @@
 package com.billt.core.notificationservice.Services;
 
-import com.billt.core.datasourcebase.model.invoiceReceiver.TransactionFlowRequestBean;
-import com.billt.core.notificationservice.Models.NotificationRequestModel;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-
 @Service
 public class EmailSender{
     @Autowired
     public JavaMailSender emailSender;
+
     public EmailSender(){
     }
 
